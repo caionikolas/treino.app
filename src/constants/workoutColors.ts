e9bcf0c59a4f32dfe504a935ee7304274986fd8a
Nikolas <caio.kanu@hotmail.com>
2026-04-18 02:03:46 -0300

    feat: add workout color palette

---
+++ src/constants/workoutColors.ts
@@ -0,0 +1,14 @@
+export const WORKOUT_COLORS = [
+  '#E94560',
+  '#3282B8',
+  '#6A4C93',
+  '#F39C12',
+  '#00B894',
+  '#FD79A8',
+  '#FDCB6E',
+  '#A29BFE',
+  '#00CEC9',
+  '#E17055',
+] as const;
+
+export const DEFAULT_WORKOUT_COLOR = WORKOUT_COLORS[0];
